f7877a4da7bad76c2799eff17711ab09fe5d6d8a
Evgenii17web <evgenii17pershin@gmail.com>
2022-11-20 22:35:45 +0200

    Add "gulp-imagemin" to package.json. Images optimization

--- task/img.js
+++ task/img.js
@@ -1,10 +1,12 @@
 import gulp from "gulp";
 import path from "../config/path.js";
 import webp from "gulp-webp";
+import imagemin from "gulp-imagemin";
 
 const img = () => {
     return gulp.src(path.img.src)
         .pipe(gulp.dest(path.img.dest))
+        .pipe(imagemin())
         .pipe(webp())
         .pipe(gulp.dest(path.img.dest));
 }
